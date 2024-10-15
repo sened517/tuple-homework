85954fd81c62569059c20753c2dc237651cf64c2
sened517 <seha56@yahoo.com>
2024-10-15 18:10:59 +0100

    Add files via upload

---
+++ tuple2.py
@@ -0,0 +1,14 @@
+
+brands=("nike","addias","puma","sondico","primark","Jordan","prada","shien","under amour","essentials")
+print(brands[0:5])
+print(brands[1:5])
+print(brands[4:6])
+print(brands[0:])
+print(brands[:10])
+
+print("prada"in brands)
+print("nike"in brands)
+
+# brands=("nike","addias","puma","sondico","primark","Jordan","prada","shien","under amour","essentials")
+# del brands
+# print(brands)
